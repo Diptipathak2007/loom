5ea27049058bd75246afca08d8923974a6ff87e0
Diptipathak2007 <diptipathak019@gmail.com>
2025-11-03 21:12:37 +0530

    onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,7 @@
 async function Page() {
   return 
     <main>
-        <h1 className="head-text ">
+        <h1 className="head-text sjsskk ">
             Onboarding Page
         </h1>
     </main>
