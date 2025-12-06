624ce59cb563d1d19f5f7fa1b47f930470bab916
Diptipathak2007 <diptipathak019@gmail.com>
2025-12-06 23:14:27 +0530

    Implement activity and user search features, and integrate Clerk webhooks for community lifecycle management.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,7 +3,9 @@ import path from "path";
 import User from "../models/user.model";
 import { connectDB } from "../mongoose";
 import { revalidatePath } from "next/cache";
-import { model } from "mongoose";
+import { model, SortOrder } from "mongoose";
+import { FilterQuery } from "mongoose";
+import Loom from "../models/loom.model";
 interface Params{
     userId:string;
     username:string;
@@ -57,5 +59,73 @@ export async function fetchUser(userId:string){
         return user;
     } catch (error) {
         throw new Error("Failed to fetch user");
+    }
+}
+
+export async function fetchUsers({userId,pageNumber=1,sortBy="desc",pageSize=20,searchString=""}:{
+    userId:string;
+    pageNumber?:number;
+    sortBy?:SortOrder;
+    pageSize?:number;
+    searchString?:string;
+}){
+    try {
+        await connectDB();
+        const skipAmount=(pageNumber-1)*pageSize;
+        const regex=new RegExp(searchString,"i");
+        const query:FilterQuery<typeof User>={
+            id:{
+                $ne:userId
+            }
+            
+        }
+        if(searchString.trim()!==""){
+                query.$or=[
+                    {username:{$regex:regex}},
+                    {name:{$regex:regex}}
+                ]
+        }
+        const sortOptions={
+            createdAt:sortBy
+        }
+        const usersQuery=User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize)
+        const totalUsersCount=await User.countDocuments(query);
+        const users=await usersQuery.exec();
+        const isNext=totalUsersCount>skipAmount+users.length;
+        return {
+            users,
+            isNext
+        }
+    } catch (error) {
+        throw new Error("Failed to fetch users");
+    }
+}
+
+export async function getActivity(userId:string){
+    try {
+        await connectDB();
+        const userLoom=await Loom.find({author:userId})
+        const childLoomIds=userLoom.reduce((acc: any[], loom: any)=>{
+            return acc.concat(loom.children)
+        },[])
+        const replies=await Loom.find({
+            _id:{
+                $in:childLoomIds
+            },
+            author:{
+                $ne:userId
+            }
+        }).populate({
+            path:"author",
+            model:User,
+            select:"name image _id"
+        })
+        return replies;
+       
+    } catch (error) {
+        throw new Error("Failed to fetch activity");
     }
 }
